bba778a1c78b00a0312c122449a41cd536c1b859
Nicholas Sessa <nicholas.sessa@gmail.com>
2019-06-11 15:59:17 -0400

    made changes to the script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 var wins = 0;
 var losses = 0;
 
-var errors = 0;
-var maxErrors = 9;
+var strikes = 0;
+var maxStrikes = 9;
 
 var wordDisplayLettersElement = document.getElementById("word-display-letters");
 var guessedLettersElement = document.getElementById("guessed-letters");
-var errorCountElement = document.getElementById("error-count");
+var strikeCountElement = document.getElementById("strike-count");
 var winCountElement = document.getElementById("win-count");
 var lossCountElement = document.getElementById("loss-count");
 
@@ -18,56 +18,47 @@ var newGame = true;
 var blinkElements = document.getElementsByClassName("blinking");
 var resultLineElements = document.getElementsByClassName("result-line");
 
-var validGuesses = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+var validGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wordList = [
-"deathstar",
-"xwing",
-"skywalker",
-"yavin",
-"darthvader",
-"hansolo",
-"tiefighter",
-"tatooine"
-]
+	"deathstar",
+	"xwing",
+	"skywalker",
+	"yavin",
+	"darthvader",
+	"hansolo",
+	"tiefighter",
+	"tatooine"
+];
+
+var theWord = "";
 
 function initializeScreen() {
-
-//get word from the wordList array above
-
-var theWord = wordList[Math.floor(Math.random() * wordList.length)];
-console.log("the word is: " + theWord);
-
-// set the array visibleLetters to contain all false values for each element in the array
-
-for (var i = 0; i < theWord.length; i++) {
-	visibleLetters[i] = false;
-}
-
-
-// loop through the word to build the underscores for the screen.
-// store the underscores in the tempString variable.
-// also set the word to be all upper case
-
-
-var tempString = "";
-for (var i = 0; i < visibleLetters.length; i++) {
-	// if visibleLetters[i] = false, then visibleLetters[i] = "_"
-	// else visibleLetters[i] = the uppercase letter of the word
-	tempString += ((visibleLetters[i]) ? theWord.charAt(i).toUpperCase() : "_");
-	if (i < (visibleLetters.length - 1)) tempString += " ";  // this simply adds a space in the letters to make it look cool
-}
-wordDisplayLettersElement.textContent = tempString;
-
-
-errors = 0;
-guessedLetters = [];
-visibleLetters = [];
-gameOver = false;
-resultLines = "";
-
-}  // END function to initialize the screen
-
+	//get word from the wordList array above
+	theWord = wordList[Math.floor(Math.random() * wordList.length)];
+	console.log("the word is: " + theWord);
+	// set the array visibleLetters to contain all false values for each element in the array
+	for (var i = 0; i < theWord.length; i++) {
+		visibleLetters[i] = false;
+	}
+	// loop through the word to build the underscores for the screen.
+	// store the underscores in the tempString variable.
+	// also set the word to be all upper case
+	var tempString = "";
+	for (var i = 0; i < visibleLetters.length; i++) {
+		// if visibleLetters[i] = false, then visibleLetters[i] = "_"
+		// else visibleLetters[i] = the uppercase letter of the word
+		tempString += ((visibleLetters[i]) ? theWord.charAt(i).toUpperCase() : "_");
+		if (i < (visibleLetters.length - 1))
+			tempString += " "; // this simply adds a space in the letters to make it look cool
+	}
+	wordDisplayLettersElement.textContent = tempString;
+	strikes = 0;
+	guessedLetters = [];
+	visibleLetters = [];
+	gameOver = false;
+	resultLines = "";
+} // END function to initialize the screen
 
 if (newGame) {
 	initializeScreen();
@@ -75,30 +66,44 @@ if (newGame) {
 
 // get input from the user
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 	var userGuess = event.key;
+	console.log("user pressed: " + userGuess);
+	// first check to see if the key pressed is a valid key:
 
-// check to see if the key pressed is a letter that matches
-console.log("user pressed: " userGuess);
-// first check to see if the key pressed is a valid key:
+	if (validGuesses.includes(userGuess)) {
+		//if so add the letter to the list of guessed letters 
+		guessedLetters.push(userGuess);
+		console.log("user entered a valid letter!");
+		// after adding the letter display it by looping through the array
+		var tempString = "";
+		for (i = 0; i < guessedLetters.length; i++) {
+			tempString += guessedLetters[i];
+			if (i < (guessedLetters.length - 1)) tempString += " ";
+		}
+		guessedLettersElement.textContent = tempString;
 
-if (validGuesses.includes(userGuess) {
-	//if so add the letter to the list of guessed letters 
+		// Next, check to see if the key pressed is a letter that matches theWord
+
+		var foundIt = false;
+		// loop through the word 
+		for (var i = 0; i < theWord.length; i++) {
+			// check each character in theWord
+			if (theWord.charAt(i) == userGuess) {
+				foundIt = true;
+				// we found a letter and so mark that position as true in the visibleLetters array
+				visibleLetters[i] = true;
+			}
+		}
+
+		// OK, if the letter is not found then add another strike (error)
+		if (!foundIt) {
+			strikes++;
+		}
+	}
+
+	// if match, update the screen like so: wordDisplayLettersElement.textContent = tempString;
+	// if no match, increment strikes.  if 9 strikes, end the game and display: YOU LOSE!  (play Darth Vader Theme.)
+	// if win, then display: YOU WIN! and then play happy rebel theme
 }
-
-
-
-// if match, update the screen like so: wordDisplayLettersElement.textContent = tempString;
-// if no match, increment strikes.  if 9 strikes, end the game and display: YOU LOSE!  (play Darth Vader Theme.)
-// if win, then display: YOU WIN! and then play happy rebel theme
-
-}
-
-
-function checkGuess(char) {
-
-}
-
-
-
 
